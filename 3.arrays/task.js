09db1b513aa3fc3a088d2ce534e2e20ee7252e5d
Alexandr Paramonov <paramonovsasha@icloud.com>
2023-01-30 16:08:46 +0300

    дз 1и2 выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,17 @@
 function compareArrays(arr1, arr2) {
-if (arr1.length === arr2.length &&
-arr1.every((value, index) => value === arr2[index])){
-    return true;
-}
-else {
-    return false;
-}
-}
-
-
-function getUsersNamesInAgeRange(users, gender) {
-    return users.filter (item  => item.gender  === gender)
-        .reduce((acc, item, index, arr) => {
-            return acc + item.age / arr.length;
-        }, 0)
-}
+    if (arr1.length === arr2.length &&
+      arr1.every((value, index) => value === arr2[index])) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  
+  function getUsersNamesInAgeRange(users, gender) {
+    return users.filter(item => item.gender === gender)
+      .reduce((acc, item, index, arr) => {
+        return acc + item.age / arr.length;
+      }, 0)
+  }
